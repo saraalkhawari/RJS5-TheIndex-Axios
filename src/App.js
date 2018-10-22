de32e49351de72ea5b9a53f8bb1c0a891a02126d
Lailz <lailaabdulraheem@gmail.com>
2018-10-22 16:46:38 +0300

    Solution updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,54 @@ import authors from "./data.js";
 
 // Components
 import Sidebar from "./Sidebar";
+import Loading from "./Loading";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
 
+const instance = axios.create({
+  baseURL: "https://the-index-api.herokuapp.com"
+});
+
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  componentDidMount() {
+    instance
+      .get("/api/authors/")
+      .then(res => res.data)
+      .then(authors =>
+        this.setState({
+          authors: authors,
+          loading: false
+        })
+      )
+      .catch(err => console.error(err));
+  }
+
+  selectAuthor = authorID => {
+    this.setState({ loading: true });
+    instance
+      .get(`/api/authors/${authorID}/`)
+      .then(res => res.data)
+      .then(author =>
+        this.setState({
+          currentAuthor: author,
+          loading: false
+        })
+      )
+      .catch(err => console.error(err));
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   getContentView = () => {
-    if (this.state.currentAuthor) {
+    if (this.state.loading) {
+      return <Loading />;
+    } else if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
       return <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />;
