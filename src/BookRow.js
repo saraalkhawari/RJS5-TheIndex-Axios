acf2b3c627e7f169470abd0fe3b79ceabaac9082
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-11-06 19:57:38 +0300

    Sara and Farah Second Commit

--- src/BookRow.js
+++ src/BookRow.js
@@ -3,10 +3,11 @@ import React from "react";
 function BookRow(props) {
   const book = props.book;
   const authorName = props.authorName;
+  const authors = book.authors.map(author => <div>{author.name}</div>);
   return (
     <tr>
       <td>{book.title}</td>
-      <td>{authorName}</td>
+      <td>{authors}</td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
